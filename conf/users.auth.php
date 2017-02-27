8eae88839ee16215f3d20fa0383f7e90774fc221
wiki <wiki@db3d58f13ccd>
2017-02-27 12:38:27 +0000

    autocommit @ Mon Feb 27 12:38:27 UTC 2017

---
+++ conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+tadone:$1$hS2NXoDH$AbeqxXeeyotrRaq6qmzKi1:Tad One:tadone@gmail.com:admin,user
